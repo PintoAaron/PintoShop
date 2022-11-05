2de343be7bea66fd5bb20ed4de1f3cabfd95f810
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2022-11-05 16:01:41 +0000

    Setup database

--- PintoShop/settings.py
+++ PintoShop/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'PintoShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pintoshop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'macquena'
     }
 }
 
